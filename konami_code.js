6e4be894e996ba449cb06fd0a292410bebae2821
Austin Powell <austinpowell58@gmail.com>
2018-07-09 00:53:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,7 @@ const body = document.body
 
 
 let index = 0;
-body.addEventListener('keydown' , onKeyDownHandler(e));
+body.addEventListener('keydown' , onKeyDownHandler;
 function onKeyDownHandler(e) {
   const key = e.key;
  
